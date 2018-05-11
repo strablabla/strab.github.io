af392203d7efdd7aa47f14f3a932e0cb0e3117ee
aspilio <lio.chiron@google.com>
2018-05-11 10:12:57 +0200

    new pos for spheres

--- js/shoot_objects1.js
+++ js/shoot_objects1.js
@@ -13,11 +13,31 @@ var make_balls = function(){
 				var material = new THREE.MeshBasicMaterial( { map: texture } );
 				var balloon = new THREE.Mesh( geometry, material );
 				//----------------
-				var posball = 30*i
-				balloon.position.set(posball, posball, posball)
+				var posball = 10*i
+				balloon.position.set(Math.random()*posball, posball, 10*posball)
 				listballs.push(balloon)
-
 			}
-
 	return listballs
 	}
+
+  var make_explosion = function(){
+  	/*
+
+  	*/
+    var listballs = []
+  	var sizeball = 100
+  	for ( i=1; i < 4; i++ ){
+
+  				var geometry = new THREE.SphereGeometry( sizeball, 32, 32 );
+  				//var texture = new THREE.TextureLoader().load( "texture/azulejos_portugal.jpg" );
+          var texture = new THREE.TextureLoader().load( "textures/hardwood2_diffuse.jpg" );
+  				var material = new THREE.MeshBasicMaterial( { map: texture } );
+  				var balloon = new THREE.Mesh( geometry, material );
+  				//----------------
+          var posball = 30*i
+          balloon.position.set(posball, posball, posball)
+  				listballs.push(balloon)
+  			}
+
+  	return listballs
+  	}
